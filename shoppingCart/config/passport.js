901e28e1177b4c49aaf1e2af65c568d39d8b224f
Simon Phillips <simon.phillips01@gmail.com>
2017-03-31 13:36:00 +0100

    Route grouping and protection

--- shoppingCart/config/passport.js
+++ shoppingCart/config/passport.js
@@ -1,7 +1,7 @@
 var passport = require('passport');
 var User = require('../models/user');
 var LocalStrategy = require('passport-local').Strategy;
-var bcrypt = require('bcrypt-nodejs');
+//var bcrypt = require('bcrypt-nodejs');
 
 passport.serializeUser(function(user, done) {
   done(null, user.id);
