77a83dc13f3858ea9044895172f14d28c82fa227
Simon Phillips <simon.phillips01@gmail.com>
2017-06-23 14:43:33 +0100

    Saving stores and using Mixins

---
+++ Dang-thats-delicous/controllers/storeController.js
@@ -0,0 +1,12 @@
+exports.homePage = (req, res) => {
+    console.log(req.name);
+    res.render('index');
+};
+
+exports.addStore = (req, res) => {
+    res.render('editStore', { title: 'Add store' });
+}
+
+exports.createStore = (req, res) => {
+    res.json(req.body);
+}
