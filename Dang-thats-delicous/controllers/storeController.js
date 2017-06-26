fd633290964735677bc63222a77dcc7f320feea2
Simon Phillips <simon.phillips01@gmail.com>
2017-06-26 19:30:59 +0100

    Flash messages

--- Dang-thats-delicous/controllers/storeController.js
+++ Dang-thats-delicous/controllers/storeController.js
@@ -1,5 +1,7 @@
+const mongoose = require('mongoose');
+const Store = mongoose.model('Store');
+
 exports.homePage = (req, res) => {
-    console.log(req.name);
     res.render('index');
 };
 
@@ -7,6 +9,8 @@ exports.addStore = (req, res) => {
     res.render('editStore', { title: 'Add store' });
 }
 
-exports.createStore = (req, res) => {
-    res.json(req.body);
+exports.createStore = async (req, res) => {
+    const store = await (new Store(req.body)).save();
+    req.flash('success', `Successfully Created ${store.name}. Care to leave a review`);
+    res.redirect(`/store/${store.slug}`);
 }
