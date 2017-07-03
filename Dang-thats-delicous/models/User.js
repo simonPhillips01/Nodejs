544522b19e917b1e7704ef93e8b00ea2361e9751
Simon Phillips <simon.phillips01@icloud.com>
2017-07-03 14:00:07 +0100

    Creating user accounts

---
+++ Dang-thats-delicous/models/User.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.Promise = global.Promise;
+const md5 = require('md5');
+const validator = require('validator');
+const mongodbErrorHandler = require('mongoose-mongodb-errors');
+const passportLocalMongoose = require('password-local-mongoose');
+
+const userSchema = new Schema ({
+    email: {
+        type: String,
+        unique: true,
+        lowercase: true,
+        trim: true,
+        validate: [validator.isEmail, 'Invalid email Address'],
+        required: 'Please supply an email address'
+    }
+    name: String,
+    required: 'Please supply a name'
+    trim: true
+});
+
+userSchema.plugin(paasportLocalMongoose, { usernameField: 'email' });
+userSchema.plugin(mongodbErrorHandler);
+
+module.exports = mongoose.model('User', userSchema);
