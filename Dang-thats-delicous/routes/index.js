6b2bd72235211a967b476e5eb2f7d0c1d6a73c66
Simon Phillips <simon.phillips01@icloud.com>
2017-07-05 13:07:10 +0100

    JSON endpoints and creating MongoDB indexes

--- Dang-thats-delicous/routes/index.js
+++ Dang-thats-delicous/routes/index.js
@@ -47,6 +47,10 @@ router.get('/account', authController.isLoggedIn, userController.account);
 router.post('/account', catchErrors(userController.updateAccount));
 router.post('/account/forgot', catchErrors(authController.forgot));
 router.get('/account/reset/:token', catchErrors(authController.reset));
-router.post('/account/reset/:token', authController.confirmedPasswords, catchErrors(authController.update))
+router.post('/account/reset/:token', authController.confirmedPasswords, catchErrors(authController.update));
+
+/* API */
+
+router.get('/api/search', catchErrors(storeController.searchStores));
 
 module.exports = router;
