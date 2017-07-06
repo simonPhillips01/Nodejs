b4e081d5250f1b98de2ffd66d03bd3bfd18c06d6
Simon Phillips <simon.phillips01@icloud.com>
2017-07-06 11:24:05 +0100

    Creating a geospatial ajax endpoint

--- Dang-thats-delicous/models/Store.js
+++ Dang-thats-delicous/models/Store.js
@@ -44,7 +44,9 @@ author: {
 storeSchema.index({
     name: 'text',
     description: 'text'
-})
+});
+
+storeSchema.index({ location: '2dsphere' });
 
 storeSchema.pre('save', async function(next) {
     if (!this.isModified('name')) {
