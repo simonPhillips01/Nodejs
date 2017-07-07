734be80f3f2d92b243fd9db6ba2ff5c9686aa827
Simon Phillips <simon.phillips01@icloud.com>
2017-07-07 13:43:04 +0100

    Displaying our heyarted stores

--- Dang-thats-delicous/routes/index.js
+++ Dang-thats-delicous/routes/index.js
@@ -49,6 +49,7 @@ router.post('/account/forgot', catchErrors(authController.forgot));
 router.get('/account/reset/:token', catchErrors(authController.reset));
 router.post('/account/reset/:token', authController.confirmedPasswords, catchErrors(authController.update));
 router.get('/map', storeController.mapPage);
+router.get('/hearts', authController.isLoggedIn, catchErrors(storeController.getHearts));
 
 /* API */
 
